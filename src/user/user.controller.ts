279f512a552642607f56fc4bfbe25fbf205fbae3
Danilo Nunes <danilowebdev@gmail.com>
2024-08-07 21:52:05 -0300

    seccond

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { User } from './interfaces/user.interface';
 
 @Controller('user')
 export class UserController {
+    constructor(private readonly userService: UserService) {}
+
     @Get()
-    async getAllUsers(){
-        return JSON.stringify({test:'abc'})
+    async getAllUsers(): Promise<User[]> {
+        return this.userService.getAllUsers();
     }
     
     @Post()
-    async createUser(
-        @Body() createUser: CreateUserDto
-    ) {
-        return createUser;
+    async createUser( @Body() createUser: CreateUserDto): Promise<User> {
+        return this.userService.createUser(createUser);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { User } from './interfaces/user.interface';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    private users: User[] = [];
+
+    async createUser(createUserDto: CreateUserDto): Promise<User> {
+
+        const saltOrRounds = 10;
+
+        const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+
+        const user: User = {
+            id: this.users.length + 1,
+            ...createUserDto,
+            password: passwordHashed,
+        }
+
+        this.users.push(user);
+
+        return user;
+    }
+
+    async getAllUsers(): Promise<User[]> {
+        return this.users;
+    }
+}
